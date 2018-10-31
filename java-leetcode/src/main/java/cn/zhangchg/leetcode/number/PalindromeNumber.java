a04a16211215a7a50f60d317d0ebe9481a70b225
zcg741 <zhangchengang741@gmail.com>
2018-10-31 16:00:20 +0800

    时候是回文数

---
+++ java-leetcode/src/main/java/cn/zhangchg/leetcode/number/PalindromeNumber.java
@@ -0,0 +1,44 @@
+package cn.zhangchg.leetcode.number;
+
+/**
+ * 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: 121
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入: -121
+ * 输出: false
+ * 解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+ * 示例 3:
+ * <p>
+ * 输入: 10
+ * 输出: false
+ * 解释: 从右向左读, 为 01 。因此它不是一个回文数。
+ *
+ * @author 张晨刚
+ */
+public class PalindromeNumber {
+
+    public static void main(String[] args) {
+        System.err.println(isPalindrome(100));
+        System.err.println(isPalindrome(101));
+        System.err.println(isPalindrome(004));
+        System.err.println(isPalindrome(-4232));
+        System.err.println(isPalindrome(232));
+    }
+
+    public static boolean isPalindrome(int x) {
+        if (x < 0 || (x != 0 && x % 10 == 0)) {
+            return false;
+        }
+        int y = 0;
+        while (y < x) {
+            y = y * 10 + x % 10;
+            x = x / 10;
+        }
+        return y == x || x == y / 10;
+    }
+}
