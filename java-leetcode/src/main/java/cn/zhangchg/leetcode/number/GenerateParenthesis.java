290a8d1a32e744d2a974e7d441f50a4aa22317fb
zcg741 <zhangchengang741@gmail.com>
2018-11-27 16:52:39 +0800

    生成括号

--- java-leetcode/src/main/java/cn/zhangchg/leetcode/number/GenerateParenthesis.java
+++ java-leetcode/src/main/java/cn/zhangchg/leetcode/number/GenerateParenthesis.java
@@ -21,11 +21,27 @@ import java.util.function.Consumer;
 public class GenerateParenthesis {
 
     public static void main(String[] args) {
-        //System.err.println(generateParenthesis(1));
-        //System.err.println(generateParenthesis(2));
-        //System.err.println(generateParenthesis(3));
-        //System.err.println(generateParenthesis(4));
+        System.err.println(generateParenthesis(1));
+        System.err.println(generateParenthesis(2));
+        System.err.println(generateParenthesis(3));
+        System.err.println(generateParenthesis(4));
 
+    }
+
+    public static List<String> generateParenthesis1(int n) {
+        List<String> ans = new ArrayList();
+        if (n == 0) {
+            ans.add("");
+        } else {
+            for (int c = 0; c < n; ++c) {
+                for (String left : generateParenthesis(c)) {
+                    for (String right : generateParenthesis(n - 1 - c)) {
+                        ans.add("(" + left + ")" + right);
+                    }
+                }
+            }
+        }
+        return ans;
     }
 
     public static List<String> generateParenthesis(int n) {
