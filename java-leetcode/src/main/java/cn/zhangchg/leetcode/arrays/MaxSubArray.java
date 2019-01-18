cd3e651219d1e53d92fc62fdb3c5318416e8a319
zcg741 <zhangchengang741@gmail.com>
2019-01-18 10:50:31 +0800

    最大子序和

--- java-leetcode/src/main/java/cn/zhangchg/leetcode/arrays/MaxSubArray.java
+++ java-leetcode/src/main/java/cn/zhangchg/leetcode/arrays/MaxSubArray.java
@@ -1,6 +1,7 @@
 package cn.zhangchg.leetcode.arrays;
 
 /**
+ *
  * 最大子序和
  * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
  * <p>
@@ -11,7 +12,8 @@ package cn.zhangchg.leetcode.arrays;
  * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
  * 进阶:
  * <p>
- * 如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
+ * 如果你已经实现复杂度为 O(n) 的解法，
+ * TODO 尝试使用更为精妙的分治法求解。
  *
  * @author 张晨刚
  */
@@ -25,7 +27,7 @@ public class MaxSubArray {
 
     private static int maxSubArray(int[] nums) {
         int maxNum = nums[0];
-        if (nums.length == 0) {
+        if (nums.length == 1) {
             return maxNum;
         }
         int sumNum = maxNum;
