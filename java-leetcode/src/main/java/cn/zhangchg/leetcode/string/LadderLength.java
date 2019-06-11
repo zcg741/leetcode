f6cf99312f0a1a58823a730ac3b5750450b85702
zhangchg <chengang.zhang@aiyingli.com>
2019-06-11 16:09:49 +0800

    copy

--- java-leetcode/src/main/java/cn/zhangchg/leetcode/string/LadderLength.java
+++ java-leetcode/src/main/java/cn/zhangchg/leetcode/string/LadderLength.java
@@ -57,11 +57,84 @@ public class LadderLength {
         wordList.add("log");
         wordList.add("cog");
         wordList.add("cot");
-        System.err.println(ladderLength.ladderLength("qa", "sq", new ArrayList<>(Arrays.asList("sa","sq","ba","sb"))));
-        //System.err.println(ladderLength.ladderLength("qa", "sq", new ArrayList<>(Arrays.asList("si", "go", "se", "cm", "so", "ph", "mt", "db", "mb", "sb", "kr", "ln", "tm", "le", "av", "sm", "ar", "ci", "ca", "br", "ti", "ba", "to", "ra", "fa", "yo", "ow", "sn", "ya", "cr", "po", "fe", "ho", "ma", "re", "or", "rn", "au", "ur", "rh", "sr", "tc", "lt", "lo", "as", "fr", "nb", "yb", "if", "pb", "ge", "th", "pm", "rb", "sh", "co", "ga", "li", "ha", "hz", "no", "bi", "di", "hi", "qa", "pi", "os", "uh", "wm", "an", "me", "mo", "na", "la", "st", "er", "sc", "ne", "mn", "mi", "am", "ex", "pt", "io", "be", "fm", "ta", "tb", "ni", "mr", "pa", "he", "lr", "sq", "ye"))));
+        //System.err.println(ladderLength.ladderLength("qa", "sq", new ArrayList<>(Arrays.asList("sa","sq","ba","sb"))));
+        System.err.println(ladderLength.ladderLength("qa", "sq", new ArrayList<>(Arrays.asList("si", "go", "se", "cm", "so", "ph", "mt", "db", "mb", "sb", "kr", "ln", "tm", "le", "av", "sm", "ar", "ci", "ca", "br", "ti", "ba", "to", "ra", "fa", "yo", "ow", "sn", "ya", "cr", "po", "fe", "ho", "ma", "re", "or", "rn", "au", "ur", "rh", "sr", "tc", "lt", "lo", "as", "fr", "nb", "yb", "if", "pb", "ge", "th", "pm", "rb", "sh", "co", "ga", "li", "ha", "hz", "no", "bi", "di", "hi", "qa", "pi", "os", "uh", "wm", "an", "me", "mo", "na", "la", "st", "er", "sc", "ne", "mn", "mi", "am", "ex", "pt", "io", "be", "fm", "ta", "tb", "ni", "mr", "pa", "he", "lr", "sq", "ye"))));
+    }
+    /*
+     * 执行用时 : 65 ms, 在Word Ladder的Java提交中击败了90.05% 的用户
+     * 内存消耗 : 37.4 MB, 在Word Ladder的Java提交中击败了93.40% 的用户
+     * 这个解法是抄热评第一的，我之前以为就是个回溯问题，结果是bfs，对bfs还不太熟悉。
+     */
+    public int ladderLength(String beginWord, String endWord,
+                            List<String> wordList) {
+        /*
+         * 这里是为了排除endWord不在wordList中的情况，不然下面遍历的时候endWord变化一位就
+         * 在wordList中了。
+         */
+        if (!wordList.contains(endWord)) {
+            return 0;
+        }
+        /*
+         * bfs就是把每次筛选出来的节点保存到一个Set中，然后再次逐个遍历
+         */
+        Set<String> beginSet = new HashSet<String>();
+        Set<String> endSet = new HashSet<String>();
+        beginSet.add(beginWord);
+        endSet.add(endWord);
+        int step = 1;
+        while (beginSet.size() > 0) {
+            for (String b : beginSet) {
+                wordList.remove(b);
+            }
+            step ++;
+            Set<String> tempSet = new HashSet<>();
+            for (String b : beginSet) {
+                for (Iterator<String> iterator = wordList.iterator();
+                     iterator.hasNext(); ) {
+                    String w = iterator.next();
+                    if (diff(b, w) > 1) {
+                        continue;
+                    } else {
+                        if (endSet.contains(w)) {
+                            return step;
+                        } else {
+                            tempSet.add(w);
+                            /*
+                             * 本来想提前遍历删除的，结果失败了
+                             * 这里也要注意，在遍历List的时候删除有两种办法：
+                             * 1. iterator 这里采用此种方法
+                             * 2. 倒序遍历删除
+                             */
+//                            wordList.remove(w);
+//                            iterator.remove();
+                        }
+                    }
+                }
+            }
+            /*
+             * 这里的tempSet其实是nextBeginSet, 判断一下从哪边走简单就从哪边走
+             */
+            if (tempSet.size() > endSet.size()) {
+                beginSet = endSet;
+                endSet = tempSet;
+            } else {
+                beginSet = tempSet;
+            }
+        }
+        return 0;
     }
 
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+    private int diff(String w1, String w2) {
+        int deviation = 0;
+        for (int i = 0; i < w1.length() && deviation < 2; ++i) {
+            if (w1.charAt(i) != w2.charAt(i)) {
+                ++deviation;
+            }
+        }
+        return deviation;
+    }
+
+    public int ladderLength2(String beginWord, String endWord, List<String> wordList) {
         if (!wordList.contains(endWord)) {
             return 0;
         }
