d0f987bc16f23553a479f9f3b09305afa6def8d0
zhangchg <chengang.zhang@aiyingli.com>
2019-06-19 12:55:40 +0800

    189、旋转数组

--- java-script-leetcode/src/rotate.js
+++ java-script-leetcode/src/rotate.js
@@ -31,23 +31,72 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var rotate = function(nums, k) {
-  for (let i = 0; i < k; i++) {
-    let first = nums[0];
-    nums[0] = nums[nums.length - 1];
-    for (let j = 1; j < nums.length; j++) {
-      var flag = nums[j];
-      nums[j] = first;
-      first = flag;
+var rotate = function (nums, k) {
+  if(k > nums.length){
+    k = k % nums.length;
+  }
+  var k0 = 0, kk = nums.length - k - 1, k1 = nums.length - k, k2 = nums.length - 1;
+  while (k0 < kk) {
+    let flag = nums[k0];
+    nums[k0] = nums[kk];
+    nums[kk] = flag;
+    kk--;
+    k0++;
+  }
+
+  while (k1 < k2) {
+    let flag = nums[k1];
+    nums[k1] = nums[k2];
+    nums[k2] = flag;
+    k2--;
+    k1++;
+  }
+  k0 = 0, k2 = nums.length - 1;
+  while (k0 < k2) {
+    let flag = nums[k0];
+    nums[k0] = nums[k2];
+    nums[k2] = flag;
+    k2--;
+    k0++;
+  }
+};
+var rotate1 = function(nums, k) {
+  k = k % nums.length;
+  if (k === 0) return;
+  let start = 0;
+  let next = start;
+  let value = nums[start];
+  let count = gcd(nums.length, k);
+  while (start < count) {
+    next =  (next + k) % nums.length;
+    let tmp = nums[next];
+    nums[next] = value;
+    value = tmp;
+    if (next === start) {
+      start += 1;
+      next = start;
+      value = nums[start];
     }
   }
 };
+
+// 求两个正整数a和b的最大公约数
+var gcd = function(a, b) {
+  let r;
+  while ((r = a % b) !== 0) {
+    a = b;
+    b = r;
+  }
+  return b;
+};
+
+
 /**
  * @param {number[]} nums
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var rotate2 = function(nums, k) {
+var rotate2 = function (nums, k) {
   for (let i = 0; i < k; i++) {
     let first = nums[0];
     nums[0] = nums[nums.length - 1];
@@ -59,9 +108,9 @@ var rotate2 = function(nums, k) {
   }
 };
 
-var num = [1,2,3,4,5,6,7];
-rotate(num,3)
+var num = [1, 2, 3, 4, 5, 6, 7];
+rotate(num, 4)
 console.log(num);
-num = [-1,-100,3,99];
-rotate(num,2)
+num = [-1, -100, 3, 99];
+rotate(num, 2)
 console.log(num);
