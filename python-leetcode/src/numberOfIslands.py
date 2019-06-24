69b2fe4a8f72828d82d1931d2f709faac07962ca
zhangchg <chengang.zhang@aiyingli.com>
2019-06-24 18:31:37 +0800

    200、岛屿数量

--- python-leetcode/src/numberOfIslands.py
+++ python-leetcode/src/numberOfIslands.py
@@ -26,6 +26,35 @@
 
 visited = []
 class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        m = len(grid)
+        if m == 0:
+            return 0
+        n = len(grid[0])
+
+        count = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "1":
+                    count += 1
+                    self.change(grid,i,j)
+        return count
+
+    def change(self, grid,i,j):
+        grid[i][j] = "0"
+        if i > 0 and grid[i - 1][j] == "1":
+            self.change(grid,i-1,j)
+        if j > 0 and grid[i][j - 1] == "1":
+            self.change(grid,i,j-1)
+        if i < len(grid) - 1 and grid[i + 1 ][j] == "1":
+            self.change(grid,i + 1,j)
+        if j < len(grid[0]) - 1 and grid[i][j + 1] == "1":
+            self.change(grid,i,j + 1)
+
     def dfs(self, grid, i, j):
         """
         :type grid: List[List[str]]
@@ -40,7 +69,7 @@ class Solution(object):
                 self.dfs(grid, i, j + 1)
                 self.dfs(grid, i, j - 1)
 
-    def numIslands(self, grid):
+    def numIslands2(self, grid):
         """
         :type grid: List[List[str]]
         :rtype: int
